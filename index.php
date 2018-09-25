3ae92b812b5b7e3f9cd50a496f29afa8581988fb
dannehtran <danny.tran.392@my.csun.edu>
2018-09-25 10:45:59 -0700

    Updated navbar on index.php

--- index.php
+++ index.php
@@ -7,7 +7,51 @@
   <meta name="keywords" content="Cerulean, Shipping, Tracking, Drones">
 </head>
 <body>
-  <div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Shipping
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Tracking
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <form>
+              <input type="tracking" class="form-control" id="trackingNumber1" placeholder="Tracking ID">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">More Tracking Information</a>
+            </div>
+          </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Log In or Sign Up
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <form>
+              <input type="username" class="form-control" id="userID1" placeholder="User ID">
+              <input type="password" class="form-control" id="password1" placeholder="Password">
+              <button type="submit" class="btn btn-primary">LOGIN</button>
+            </form>
+            <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Don't Have An Account?</a>
+            </div>
+          </li>
+      </ul>
   </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
