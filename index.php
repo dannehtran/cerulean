cb8f3b7f870141b3b1ba4eb7a279cd684f263924
dannehtran <danny.tran.392@my.csun.edu>
2018-09-28 12:45:47 -0700

    Updated with image of drone and added more padding

--- index.php
+++ index.php
@@ -8,6 +8,17 @@
     .jumbotron {
       text-align: center;
     }
+    .image-padding {
+      padding-left: 30px;
+    }
+    .image-resize {
+      width: 450;
+      height: 300;
+    }
+    .description {
+      padding: 15px;
+      text-align: center;
+    }
   </style>
   <title>Cerulean Shipping Company</title>
   <meta charset="UTF-8">
@@ -39,7 +50,7 @@
             </div>
           </li>
         <li class="nav-item">
-          <a class="nav-link" id="contactUsNav" role="button" href="#">Contact Us</a>
+          <a class="nav-link" id="contactUsNav" role="button" href="#">Locations</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -60,11 +71,32 @@
   </nav>
 <!-- END OF NAV BAR -->
 <!-- START OF HOMEPAGE -->
+  <!-- CERLUEAN SHIPPING JUMBOTRON -->
   <div class="jumbotron" id="backgroundStory">
       <h1 class="jumboHeader">Cerulean Shipping</h1>
       <p>We are a mid-sized shipping company that provides services over all terrain such as land, sea, and air. We plan on implementing new technologies that are newer to the shipping industry such as drone shipping and GPS location delivery. This can help put us one step ahead on faster delivery times, and change the shipping industry.</p>
       <p><a class="btn btn-primary" href="#">Learn More</a></p>
   </div>
+  <!-- DRONE IMAGE-->
+  <div class="container-fluid image-padding shadow rounded">
+    <div class="row">
+      <div class="col">
+        <img class="image-resize" src="images/drone_shipp.PNG" alt="drone">
+      </div>
+      <div class="col">
+        <img class="image-resize" src="#" alt="truck">
+      </div>
+      <div class="col">
+        <img class="image-resze" src="#" alt="ship">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col description">Done shipping is our newest shipping technology. We carry out your standard shipment with on the go GPS tracking drone services. Depending on your location, your delivery can be made within a few hours after your order/label has been placed.</div>
+      <div class="col description"></div>
+      <div class="col description"></div>
+    </div>
+  </div>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
