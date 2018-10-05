65e7c6e9d30a415d9f77e61280661f5fa5f95b0e
dannehtran <danny.tran.392@my.csun.edu>
2018-10-05 12:51:39 -0700

    Updated index.php with PHP code and implemented external php files

--- index.php
+++ index.php
@@ -9,59 +9,10 @@
   <meta name="description" content="We are a shipping company that provide services throughout the United States.">
   <meta name="keywords" content="Cerulean, Shipping, Tracking, Drones">
 </head>
-<body>
-
-  <!-- THIS IS THE NAV BAR -->
-  <nav class="navbar navbar-expand-lg navbar-dark fixed-top navBarCustom">
-
-  <!-- THIS IS THE DIV THAT CONTAINS ALL THE TABS FOR THE NAV BAR -->
-  <div class="collapse navbar-collapse justify-content-center container" id="navbarSupportedContent">
-    <ul class="navbar-nav">
-      <li class="nav-item dropdown">
-        <a class="nav-link" href="index.php" id="HomeNav" role="button">
-          Home
-        </a>
-        </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link" href="shipping.php" id="shippingNav" role="button">
-          Shipping
-        </a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Tracking
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <form>
-              <input type="tracking" class="form-control" id="trackingNumber1" placeholder="Tracking ID">
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-            <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">More Tracking Information</a>
-            </div>
-          </li>
-        <li class="nav-item">
-          <a class="nav-link" id="contactUsNav" role="button" href="contact_us.php">Locations</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Log In or Sign Up
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <form>
-              <input type="username" class="form-control" id="userID1" placeholder="User ID">
-              <input type="password" class="form-control" id="password1" placeholder="Password">
-              <button type="submit" class="btn btn-primary">LOGIN</button>
-            </form>
-            <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Don't Have An Account?</a>
-            </div>
-          </li>
-      </ul>
-  </div>
-  </nav>
-<!-- END OF NAV BAR -->
-
+<!-- HEADER -->
+<?php
+  include "header.php";
+ ?>
 <!-- START OF HOMEPAGE -->
 
   <!-- CERLUEAN SHIPPING JUMBOTRON -->
@@ -121,18 +72,7 @@
     </div>
   </div>
 
-  <!-- START OF FOOTER-->
-  <footer>
-
-    <!-- Copyright -->
-    <div class="footer-copyright py-3 copyright">© 2018 Copyright:
-      <a href="#">Ceruleanshipping.com</a>
-    </div>
-  </footer>
-
-  <!-- END OF FOOTER-->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-</html>
+  <!-- FOOTER -->
+<?php
+  include "footer.php";
+?>
