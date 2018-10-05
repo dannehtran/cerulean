082724ec97036a69ab0dd0439ff7b6f1eb36053f
dannehtran <danny.tran.392@my.csun.edu>
2018-10-05 12:50:52 -0700

    Made a footer so we can include it in every php file

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php
+  session_start();
+?>
+<!-- START OF FOOTER-->
+<footer>
+
+  <!-- Copyright -->
+  <div class="footer-copyright py-3 copyright">© 2018 Copyright:
+    <a href="#">Ceruleanshipping.com</a>
+  </div>
+</footer>
+
+<!-- END OF FOOTER-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
