644d7479ab782a08f0f8c0b93ed92bb15ccd08d6
dannehtran <danny.tran.392@my.csun.edu>
2018-10-09 13:38:10 -0700

    added comments

--- contact_us.php
+++ contact_us.php
@@ -4,7 +4,14 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/index.css">
 	<title>Contact Us</title>
-
+<!-- PLEASE USE CLASS/ID's to use CSS, in case we have multiple tables, th's, td's, etc-->
+<!-- MOVE THIS STYLE BRACKET INTO THE INDEX.CSS SHEET WITH THE NEW FORMAT MENTIONED ABOVE -->
+  <style>
+    table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    }
+  </style>
 </head>
 <body>
 	<body>
@@ -74,53 +81,51 @@
         <p>For appropriate and quick service please contact the appropriate branch. </p>
   			</header>
 
-        <div>
-    			<table class="contactTable">
-            <tr>
-                <th>Branch Office</th>
-                <th>Email</th>
-                <th>Phone Number</th>
-                <th>Address</th>
-            </tr>
-            <tr>
-              <td>Cerulean Ground</td>
-              <td>cerulean.ground.support@gmail.com</td>
-              <td>1.800.464.1020</td>
-              <td>600 East Brokaw <br> San Jose, CA 95112 USA</td>
-            </tr>
-            <tr>
-              <td>Cerulean Air Mail</td>
-              <td>cerulean.air.support@gmail.com</td>
-              <td>1.800.464.2030</td>
-              <td>6845 South Las Vegas Blvd<br>Las Vegas, NV 89119 USA</td>
-            </tr>
-            <tr>
-              <td>Cerulean Freight</td>
-              <td>cerulean.frieght.support@gmail.com</td>
-              <td>1.800.464.4050</td>
-              <td>12710 Executive Dr.<br>Dallas, TX 75238</td>
-            </tr>
-            <tr>
-              <td>Cerulean Drone</td>
-              <td>cerulean.drone.support@gmail.com</td>
-              <td>1.800.464.6070</td>
-              <td>800 Garden Ave. North<br>Renton, WA 98057</td>
-            </tr>
-            <tr>
-              <td>Cerulean Head Branch</td>
-              <td>cerulean.shipping@gmail.com</td>
-              <td>1.800.580.2334</td>
-              <td>3296 N.W. Commerce Ave.<br>Duluth, GA 30096</td>
-            </tr>
-          </table>
-        </div>
+  			<table>
+          <tr>
+              <th>Branch Office</th>
+              <th>Email</th>
+              <th>Phone Number</th>
+              <th>Address</th>
+          </tr>
+          <tr>
+            <td>Cerulean Ground</td>
+            <td>cerulean.ground.support@gmail.com</td>
+            <td>1.800.464.1020</td>
+            <td>600 East Brokaw <br> San Jose, CA 95112 USA</td>
+          </tr>
+          <tr>
+            <td>Cerulean Air Mail</td>
+            <td>cerulean.air.support@gmail.com</td>
+            <td>1.800.464.2030</td>
+            <td>6845 South Las Vegas Blvd<br>Las Vegas, NV 89119 USA</td>
+          </tr>
+          <tr>
+            <td>Cerulean Freight</td>
+            <td>cerulean.frieght.support@gmail.com</td>
+            <td>1.800.464.4050</td>
+            <td>12710 Executive Dr.<br>Dallas, TX 75238</td>
+          </tr>
+          <tr>
+            <td>Cerulean Drone</td>
+            <td>cerulean.drone.support@gmail.com</td>
+            <td>1.800.464.6070</td>
+            <td>800 Garden Ave. North<br>Renton, WA 98057</td>
+          </tr>
+          <tr>
+            <td>Cerulean Head Branch</td>
+            <td>cerulean.shipping@gmail.com</td>
+            <td>1.800.580.2334</td>
+            <td>3296 N.W. Commerce Ave.<br>Duluth, GA 30096</td>
+          </tr>
+        </table>
 		</article>
   </div>
  <!--end of contact page content-->
  <!-- START OF FOOTER-->
  <footer>
 	 <!-- Copyright -->
-	 <div class="footer-copyright py-3 copyright">© 2018 Copyright:
+	 <div class="footer-copyright py-3 copyright fixed-bottom">© 2018 Copyright:
 		 <a href="#">Ceruleanshipping.com</a>
 	 </div>
  </footer>
