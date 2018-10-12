acc4c3ede25ac3fc2efade266687bb232628dc13
NoelH <noel.hernandez.501@my.csun.edu>
2018-10-12 16:04:02 -0700

    changed php header

---
+++ locations.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+  <link rel="stylesheet" type="text/css" href="css/locations.css">
+	<title>Cerulean Locations</title>
+
+</head>
+<!--Header-->
+<?php
+include "header.php"
+?>
+<!--header end -->
+
+ <!--contact page content-->
+  <div>
+  		<article>
+  			<header>
+    		<h1>Cerulean Shipping</h1>
+    		<h3>How to Contact Us</h3>
+    		<p>Reach us at any of our main branches.</p>
+        <p>For appropriate and quick service please contact the appropriate branch. </p>
+  			</header>
+
+        <div>
+    			<table class="contactTable">
+            <tr>
+                <th>Branch Office</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+                <th>Address</th>
+            </tr>
+            <tr>
+              <td>Cerulean Ground</td>
+              <td>cerulean.ground.support@gmail.com</td>
+              <td>1.800.464.1020</td>
+              <td>600 East Brokaw <br> San Jose, CA 95112 USA</td>
+            </tr>
+            <tr>
+              <td>Cerulean Air Mail</td>
+              <td>cerulean.air.support@gmail.com</td>
+              <td>1.800.464.2030</td>
+              <td>6845 South Las Vegas Blvd<br>Las Vegas, NV 89119 USA</td>
+            </tr>
+            <tr>
+              <td>Cerulean Freight</td>
+              <td>cerulean.frieght.support@gmail.com</td>
+              <td>1.800.464.4050</td>
+              <td>12710 Executive Dr.<br>Dallas, TX 75238</td>
+            </tr>
+            <tr>
+              <td>Cerulean Drone</td>
+              <td>cerulean.drone.support@gmail.com</td>
+              <td>1.800.464.6070</td>
+              <td>800 Garden Ave. North<br>Renton, WA 98057</td>
+            </tr>
+            <tr>
+              <td>Cerulean Head Branch</td>
+              <td>cerulean.shipping@gmail.com</td>
+              <td>1.800.580.2334</td>
+              <td>3296 N.W. Commerce Ave.<br>Duluth, GA 30096</td>
+            </tr>
+          </table>
+        </div>
+		</article>
+  </div>
+ <!--end of contact page content-->
+
+<?php
+include "footer.php"
+?>
