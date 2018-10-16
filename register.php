50b309172efd13dde023c3463e7b91967c6de0d4
dannehtran <danny.tran.392@my.csun.edu>
2018-10-16 15:10:48 -0700

    added commits for rows and input fields

--- register.php
+++ register.php
@@ -2,10 +2,16 @@
 <?php
   include_once "header.php";
  ?>
+
+ <!-- Sign Up Form -->
  <div class="container signUpForm shadow rounded my-5 px-5">
  <form action="handlers/registerHandler.php" class="needs-validation py-4 px-4" novalidate>
    <h1 class="text-center">Sign Up Form</h1>
+
+   <!-- Row 1 -->
    <div class="form-row justify-content-center">
+
+     <!-- Username Input Field -->
      <div class="col-md-5 mb-3">
        <label for="validationCustom01">Username</label>
        <input type="text" class="form-control" name="username_reg" id="validationCustom01" placeholder="Username" required>
@@ -13,6 +19,8 @@
          Please enter a username.
        </div>
      </div>
+
+     <!-- Password Input Field -->
      <div class="col-md-5 mb-3">
        <label for="validationCustom02">Password</label>
        <input type="password" class="form-control" name="password_reg" id="validationCustom02" placeholder="Password" required>
@@ -21,7 +29,11 @@
        </div>
      </div>
    </div>
+
+   <!-- Row 2 -->
    <div class="form-row justify-content-center">
+
+     <!-- Email Input Field -->
      <div class="col-md-5 mb-3">
        <label for="validationCustom03">Email</label>
        <input type="text" class="form-control" name="email_reg" id="validationCustom03" placeholder="Email" required>
@@ -29,6 +41,8 @@
          Please enter your email address.
        </div>
      </div>
+
+     <!-- Confirm Password Input Field -->
      <div class="col-md-5 mb-3">
        <label for="validationCustom04">Re-type password</label>
        <input type="password" class="form-control" name="password2_reg" id="validationCustom04" placeholder="Confirm Password" required>
@@ -37,7 +51,11 @@
        </div>
      </div>
    </div>
+
+   <!-- Row 3 -->
    <div class="form-row justify-content-center">
+
+     <!-- First name Input Field -->
      <div class="col-md-5 mb-3">
        <label for="validationCustom05">First name</label>
        <input type="text" class="form-control" name="first_name_reg" id="validationCustom05" placeholder="First name" required>
@@ -45,6 +63,8 @@
          Please enter your first name.
        </div>
      </div>
+
+     <!-- Lastname Input Field -->
      <div class="col-md-5 mb-3">
        <label for="validationCustom06">Last name</label>
        <input type="text" class="form-control" name="last_name_reg" id="validationCustom06" placeholder="Last name" required>
@@ -53,7 +73,11 @@
        </div>
      </div>
    </div>
+
+   <!-- Row 4 -->
    <div class="form-row justify-content-center">
+
+     <!-- Address Input Field -->
      <div class="col-md-6 mb-3">
        <label for="validationCustom07">Address</label>
        <input type="text" class="form-control" name="address_reg" id="validationCustom07" placeholder="Address" required>
@@ -61,19 +85,27 @@
          Please provide a valid address.
        </div>
      </div>
+
+     <!-- Address 2 Input Field -->
      <div class="col-md-4 mb-3">
        <label for="validationCustom08">Address 2 (Optional)</label>
        <input type="text" class="form-control" name="address2_reg" id="validationCustom08" placeholder="Address">
      </div>
    </div>
+
+   <!-- Row 5 -->
    <div class="form-row justify-content-center">
      <div class="col-md-6 mb-3">
+
+       <!-- City Input Field -->
        <label for="validationCustom09">City</label>
        <input type="text" class="form-control" name="city_reg" id="validationCustom09" placeholder="City" required>
        <div class="invalid-feedback">
          Please provide a valid city.
        </div>
      </div>
+
+     <!-- State Input Field -->
      <div class="col-md-4 mb-3">
        <label for="validationCustom10">State</label>
        <input type="text" class="form-control" name="state_reg" id="validationCustom10" placeholder="State" required>
@@ -82,7 +114,11 @@
        </div>
      </div>
    </div>
+
+   <!-- Row 6 -->
    <div class="form-row justify-content-center">
+
+     <!-- Phone number Input Field -->
      <div class="col-md-5 mb-3">
        <label for="validationCustom11">Phone Number</label>
        <input type="text" class="form-control" name="phone_reg" id="validationCustom11" placeholder="Phone Number" required>
@@ -90,6 +126,8 @@
          Please provide a valid phone number.
        </div>
      </div>
+
+     <!-- Zipcode Input Field -->
      <div class="col-md-5 mb-3">
        <label for="validationCustom12">Zip</label>
        <input type="text" class="form-control" name="zip_reg" id="validationCustom12" placeholder="Zip" required>
@@ -112,7 +150,12 @@
    </div>
  </form>
 </div>
+
+<!-- Validation Script -->
+
 <script src="script/validation.js"></script>
+
+<!-- Footer -->
 <?php
   include_once "footer.php";
  ?>
