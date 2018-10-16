bf6a10f377be70df809a4d84fe51c9b16729eac2
dannehtran <danny.tran.392@my.csun.edu>
2018-10-15 17:07:49 -0700

    Fixed loginHanlder code to now properly log user in and keep a session

--- handlers/loginHandler.php
+++ handlers/loginHandler.php
@@ -1,16 +1,17 @@
 <?php
-
+session_start();
 // Checks if user presses login button
 if(isset($_POST["login"])) {
   require '../db_connect.php';
-  $user_mail = $_POST['$username'];
+  $user_mail = $_POST['username'];
   $password = $_POST['password'];
+  //echo $_POST['username'] . "STRING1";
   if (empty($user_mail) || empty($password)) {
-    header("Location: ../index.php?error=emptyfields");
+    header("Location: ../index.php");
     exit();
   }
   else {
-    $sql = "SELECT * FROM customers WHERE username=? OR email=?;";
+    $sql = 'SELECT * FROM customers WHERE username=? OR email=?';
     $stmt = mysqli_stmt_init($connection);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
       header("Location: ../index.php?error=sqlError");
@@ -22,7 +23,7 @@ if(isset($_POST["login"])) {
       $result = mysqli_stmt_get_result($stmt);
       if ($row = mysqli_fetch_assoc($result)) {
         $pwdCheck = password_verify($password, $row['password']);
-        if ($pwdCheck = false) {
+        if ($pwdCheck == false) {
           header("Location: ../index.php?error=wrongpwd");
           exit();
         }
@@ -30,7 +31,7 @@ if(isset($_POST["login"])) {
           session_start();
           $_SESSION['u_name'] = $row['username'];
           $_SESSION['f_name'] = $row['firstname'];
-          header("Location: ../index.php?login=success");
+          header("Location: ../index.php?success=login");
           exit();
         }
         else {
