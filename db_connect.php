e33d038ec81d87f05e1fb9961a26de3b9d0c25c9
dannehtran <danny.tran.392@my.csun.edu>
2018-11-29 14:06:00 -0800

    Edited the php file to use cerulean_shipping as a DB

--- db_connect.php
+++ db_connect.php
@@ -5,7 +5,7 @@
 $db_host = "localhost";
 $db_username = "root";
 $db_password = "root";
-$db = "tester";
+$db = "cerulean_shipping";
 $db_port = "3306";
 
 $connection = mysqli_connect($db_host, $db_username, $db_password, $db);
