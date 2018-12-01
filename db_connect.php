c2791ffaa930397d09e0d5bf65051527ca0cea44
dannehtran <danny.tran.392@my.csun.edu>
2018-11-30 16:52:00 -0800

    made an if statement that decides if the server is hosted locally or through an ip address

--- db_connect.php
+++ db_connect.php
@@ -2,12 +2,20 @@
 
 //MySQL Database credentials
 
-$db_host = "localhost";
+$db_local = "localhost";
+$db_local_username = "root";
+$db_local_password = "root";
+$db_host = "ceruleanshipping.com";
 $db_username = "root";
-$db_password = "root";
+$db_password = "";
 $db = "cerulean_shipping";
 $db_port = "3306";
 
-$connection = mysqli_connect($db_host, $db_username, $db_password, $db);
-
+// Checks the HTTP of the host and decides if its localhost or accessible through the internet
+if ($_SERVER['HTTP_HOST'] == "localhost") {
+  $connection = mysqli_connect($db_local, $db_local_username, $db_local_password, $db);
+}
+else {
+  $connection = mysqli_connect($db_host, $db_username, $db_password, $db);
+}
 ?>
