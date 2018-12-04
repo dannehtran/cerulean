1b25d178b4a517007a707d61a68c144c4b34111c
dannehtran <danny.tran.392@my.csun.edu>
2018-12-04 15:46:11 -0800

    tracking page that displays customers tracking numbers

---
+++ mytracking.php
@@ -0,0 +1,76 @@
+<!-- HEADER -->
+<?php
+  include "header.php";
+ ?>
+
+<!-- START OF TRACKING PAGE-->
+
+<!-- TRACKING TABLE -->
+<div class="row mx-5 d-flex text-center align-items-stretch" style="padding-top:70px">
+    <div class="card shadow rounded border_color" style="width:100%">
+      <div class="card-body">
+        <h1 class="card-title">My Tracking Numbers</h1>
+          <table class="table text-center">
+            <thead class="thead-cerulean">
+              <tr>
+                <th>Tracking Number</th>
+                <th>Date Shipped</th>
+                <th>Date Received</th>
+                <th>Weight</th>
+                <th>Delivery Type</th>
+                <th>Delivered</th>
+              </tr>
+              <tbody>
+
+                <!-- PHP code to pull tracking information from the database -->
+                <?php
+                require 'db_connect.php';
+
+                // Prepares the query for information being pulled
+                $sql = 'SELECT parcel.c_id, parcel.w_id, tn, date_shipped, date_received, weight, delivered, dev_type
+                        FROM parcel
+                        INNER JOIN customer
+                        ON parcel.c_id = customer.c_id
+                        WHERE parcel.c_id = ' . $_SESSION['c_id'];
+                $query = mysqli_query($connection, $sql);
+                $result = mysqli_num_rows($query);
+
+                // Checks if there is something in the row
+                if ($result < 1 ) {
+                  echo '<h1>No Tracking Information Available!</h1>';
+                  exit();
+                }
+                else {
+
+                  //Iterates through the rows and displays them in the table
+                  while ($tracking = mysqli_fetch_assoc($query)) {
+                    if ($tracking['delivered'] == 0) {
+                        $delivery = '<span class="badge badge-pill badge-secondary">In Transit</span>';
+                    }
+                    else {
+                      $delivery = '<span class="badge badge-pill badge-success">Delivered</span>';
+                    }
+                    echo '<tr>';
+                    echo '<td><a href="tracking.php?trackNum='. $tracking['tn'] . '" name="trackSubmit" type="submit" class="btn btn-sml anchorColor">' . $tracking['tn'] . '</a></td>';
+                    echo '<td>' . $tracking['date_shipped'] . '</td>';
+                    echo '<td>' . $tracking['date_received'] . '</td>';
+                    echo '<td>' . $tracking['weight'] . ' lbs</td>';
+                    echo '<td>' . $tracking['dev_type'] . '</td>';
+                    echo '<td>' . $delivery . '</td>';
+                    echo '</tr>';
+                  }
+                }
+                ?>
+                <!-- END OF PHP CODE -->
+
+              </tbody>
+          </table>
+      </div>
+    </div>
+</div>
+<!--END OF TABLE -->
+
+<!-- FOOTER -->
+<?php
+include_once "footer.php";
+?>
