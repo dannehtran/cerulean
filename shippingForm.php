b8b3bae6d632656cf54bee5cf9f913ae23700718
NoeloleoN <noel.hernandez.501@my.csun.edu>
2018-12-04 18:38:18 -0800

    validated

--- shippingForm.php
+++ shippingForm.php
@@ -17,59 +17,94 @@ include "header.php"
 <!--header end -->
 
 <!--shipping page content-->
-<!-- Sign Up Form -->
+<!-- Shipping Form-->
  <div class="card shadow rounded border_color mx-5">
- <form method="" action="" class=" py-4 px-4">
+ <form method="POST" action="shippingForm2.php" class="needs-validation py-4 px-4" novalidate>
    <h1 class="text-center">Shipping Form</h1>
    <h4 class="text-center">Billing/Return Address</h4>
-
+<!--Row one Names-->
    <div class="form-row justify-content-center">
      <div class="col-md-3 mb-3">
-       <label>First Name</label>
-       <input type="text" class="form-control" name="firstnameBilling" id="firstnameReturn" placeholder="John" required>
-     </div>
+       <label for="validationCustom01">First Name</label>
+       <input type="text" class="form-control" name="firstnameBilling" id="validationCustom01" placeholder="John" required>
+        <div class="invalid-feedback">
+          Please enter your First Name.
+        </div>
+      </div>
 
      <div class="col-md-3 mb-3">
        <label>Last Name</label>
        <input type="txt" class="form-control" name="LastnameBilling" id="lastnameReturn" placeholder="Doe" required>
-     </div>
+      <div class="invalid-feedback">
+          Please enter your Last Name.
+      </div>
+    </div>
    </div>
+<!--END of Row one-->
 
+<!--Row two Email and Phone-->
    <div class="form-row justify-content-center">
      <div class="col-md-4 mb-3">
        <label>Email</label>
        <input type="text" class="form-control" name="emailBilling" id="emailReturn" placeholder="john.doe@cerulean.com" required>
-     </div>
+       <div class="invalid-feedback">
+          Please enter your Email.
+      </div>
+    </div>
+
      <div class="col-md-2 mb-3">
        <label>Phone Number</label>
        <input type="phone" class="form-control" name="phoneBilling" id="phoneReturn" placeholder="888-555-7777" required>
+       <div class="invalid-feedback">
+          Please enter your Phone Number.
+      </div>
      </div>
    </div>
+<!--END of row two-->
 
+<!--Row three Address-->
    <div class="form-row justify-content-center">
      <div class="col-md-6 mb-3">
        <label>Address</label>
        <input type="text" class="form-control" name="addressBilling" id="addressReturn" placeholder="Address" required>
+       <div class="invalid-feedback">
+          Please enter your Address.
+      </div>
      </div>
     </div>
+<!--END Row three-->
+
+<!--Row Four optional address -->
   <div class="form-row justify-content-center">
     <div class="col-md-6 mb-3">
       <label>Address 2 (Optional)</label>
       <input type="text" class="form-control" name="address2Billing" id="address2Return" placeholder="Address">
     </div>
   </div>
+<!--END row four-->
+
+<!--row five-->
    <div class="form-row justify-content-center">
      <div class="col-md-2 mb-3">
        <label>City</label>
        <input type="text" class="form-control" name="cityBilling" id="cityReturn" placeholder="City" required>
+       <div class="invalid-feedback">
+          Please enter your City.
+      </div>
      </div>
      <div class="col-md-3 mb-3">
        <label>State</label>
        <input type="text" class="form-control" name="stateBilling" id="stateReturn" placeholder="State" required>
+       <div class="invalid-feedback">
+          Please enter your State.
+      </div>
      </div>
      <div class="col-md-1 mb-3">
        <label>Zip</label>
        <input type="text" class="form-control" name="zipBilling" id="zipReturn" placeholder="Zip" required>
+       <div class="invalid-feedback">
+          Please enter your ZIP.
+      </div>
      </div>
    </div>
 
@@ -90,6 +125,9 @@ include "header.php"
       <div class="col-md-3 mb-3">
         <label>Package Weight </label>
         <input type="text" class="form-control" id="weight" name="weight" placeholder="Weight in lbs" required="" class="xyz">
+        <div class="invalid-feedback">
+          Please enter your Package Weight.
+      </div>
       </div>
     </div>
     <div class="form-row justify-content-center">
@@ -111,11 +149,14 @@ include "header.php"
 
 
    <div class="form-group text-center">
-     <button class="btn btn-secondary btn-rounded mt-3 buttonColor" type="" name="ShForm2" onclick="window.location='shippingForm2.php';">Continue</button>
+     <button class="btn btn-secondary btn-rounded mt-3 buttonColor" type="submit" name="shipping">Continue</button>
    </div>
  </form>
 </div> <!--CARD END-->
 <!--end of shipping page content-->
+
+<!-- validation script-->
+<script src="script/validation.js"></script>
 
 <!--Footer-->
 <?php
--- shippingForm2.php
+++ shippingForm2.php
@@ -16,67 +16,100 @@ include "header.php"
 ?>
 <!--header end -->
 
-<!-- Sign Up Form -->
+<!-- Shipping Form 2 -->
 <div class="card shadow rounded border_color mx-5">
- <form method="" action="" class=" py-4 px-4">
+ <form method="POST" action="checkout.php" class="needs-validation py-4 px-4" novalidate="">
    <h1 class="text-center">Shipping Form</h1>
    <h4 class="text-center">Delivery Address</h4>
 
+   <!--Row one Names-->
    <div class="form-row justify-content-center">
      <div class="col-md-3 mb-3">
-       <label>First Name</label>
-       <input type="text" class="form-control" name="firstnameDelivery" id="firstnameDelivery" placeholder="John" required>
-     </div>
+       <label for="validationCustom01">First Name</label>
+       <input type="text" class="form-control" name="firstnameBilling" id="validationCustom01" placeholder="John" required>
+        <div class="invalid-feedback">
+          Please enter the recipients First Name.
+        </div>
+      </div>
 
      <div class="col-md-3 mb-3">
        <label>Last Name</label>
-       <input type="txt" class="form-control" name="LastnameDelivery" id="lastnameDelivery" placeholder="Doe" required>
-     </div>
+       <input type="txt" class="form-control" name="LastnameBilling" id="lastnameReturn" placeholder="Doe" required>
+      <div class="invalid-feedback">
+          Please enter the recipients Last Name.
+      </div>
+    </div>
    </div>
+<!--END of Row one-->
 
+<!--Row two Email and Phone-->
    <div class="form-row justify-content-center">
      <div class="col-md-4 mb-3">
-       <label>Email</label>
-       <input type="text" class="form-control" name="emailDelivery" id="emailDelivery" placeholder="john.doe@cerulean.com">
-     </div>
+       <label>Email (Optional)</label>
+       <input type="text" class="form-control" name="emailBilling" id="emailReturn" placeholder="john.doe@cerulean.com">
+    </div>
+
      <div class="col-md-2 mb-3">
-       <label>Phone Number</label>
-       <input type="phone" class="form-control" name="phoneDelivery" id="phoneDelivery" placeholder="888-555-7777" required>
+       <label>Phone Number (Optional)</label>
+       <input type="phone" class="form-control" name="phoneBilling" id="phoneReturn" placeholder="888-555-7777">
      </div>
    </div>
+<!--END of row two-->
 
+<!--Row three Address-->
    <div class="form-row justify-content-center">
      <div class="col-md-6 mb-3">
        <label>Address</label>
-       <input type="text" class="form-control" name="addressDelivery" id="addressDelivery" placeholder="Address" required>
+       <input type="text" class="form-control" name="addressBilling" id="addressReturn" placeholder="Address" required>
+       <div class="invalid-feedback">
+          Please enter the recipientsAddress.
+      </div>
      </div>
     </div>
+<!--END Row three-->
+
+<!--Row Four optional address -->
   <div class="form-row justify-content-center">
     <div class="col-md-6 mb-3">
       <label>Address 2 (Optional)</label>
-      <input type="text" class="form-control" name="address2Delivery" id="address2Delivery" placeholder="Address">
+      <input type="text" class="form-control" name="address2Billing" id="address2Return" placeholder="Address">
     </div>
   </div>
+<!--END row four-->
+
+<!--row five-->
    <div class="form-row justify-content-center">
      <div class="col-md-2 mb-3">
        <label>City</label>
-       <input type="text" class="form-control" name="cityDelivery" id="cityDelivery" placeholder="City" required>
+       <input type="text" class="form-control" name="cityBilling" id="cityReturn" placeholder="City" required>
+       <div class="invalid-feedback">
+          Please enter a City.
+      </div>
      </div>
      <div class="col-md-3 mb-3">
        <label>State</label>
-       <input type="text" class="form-control" name="stateDelivery" id="stateDelivery" placeholder="State" required>
+       <input type="text" class="form-control" name="stateBilling" id="stateReturn" placeholder="State" required>
+       <div class="invalid-feedback">
+          Please enter a State.
+      </div>
      </div>
      <div class="col-md-1 mb-3">
        <label>Zip</label>
-       <input type="text" class="form-control" name="zipDelivery" id="zipDelivery" placeholder="Zip" required>
+       <input type="text" class="form-control" name="zipBilling" id="zipReturn" placeholder="Zip" required>
+       <div class="invalid-feedback">
+          Please enter a ZIP.
+      </div>
      </div>
    </div>
 
    <div class="form-group text-center">
-     <button class="btn btn-secondary btn-rounded mt-3 buttonColor" type="" name="checkout" onclick="window.location='checkout.php';">Chekout</button>
+     <button class="btn btn-secondary btn-rounded mt-3 buttonColor" type="submit" name="delivery">Chekout</button>
    </div>
  </form>
 </div> <!--CARD END-->
+
+<!-- validation script-->
+<script src="script/validation.js"></script>
 
 <!--Footer-->
 <?php
