ea606731d059448a24a6b7aae41ae4417eb3b158
NoeloleoN <noel.hernandez.501@my.csun.edu>
2019-04-23 10:19:06 -0700

    updated checkout

--- checkout.php
+++ checkout.php
@@ -15,72 +15,72 @@
 include "header.php"
 ?>
 <!--header end -->
-<div class="card shadow rounded border_color mx-5">
-<body>
-  <div class="checkout-panel">
-    <div class="panel-body">
-      <h2 class="title">Checkout</h2>
 
-      <div class="payment-method">
-        <label for="card" class="method card">
-          <div class="card-logos">
+<!--shipping page content-->
+<!-- Checkout Form-->
+ <div class="card shadow rounded border_color mx-5">
+  <form method="POST" action="shippingForm2.php" class="needs-validation py-4 px-4" novalidate>
+    <h1 class="text-center">Checkout</h1>
+
+    <div class="form-row justify-content-center"><!--Row1-->
+
+      <div class="card shadow rounded border_color mx-5 col-md-3 mb-3"> <!--Cart-->
+        <h4 class="text-center">Cart</h4>
+      </div><!--Cart End-->
+
+      <div class="card shadow rounded border_color mx-5 col-md-6 mb-3"> <!--payment-->
+        <h4 class="text-center">Payment</h4>
+
+        <div class="form-row"><!--card info Row 1-->
+          <div class="col-md-4 mb-3">
+            <label>First Name on Card</label>
+            <input type="text" class="form-control text-center" name="cardFirstName" id="" placeholder="John" required="">
+          </div>
+          <div class="col-md-4 mb-3">
+            <label>Last Name on Card</label>
+            <input type="text" class="form-control text-center" name="cardLastName" id="" placeholder="Doe" required="">
+          </div>
+          
+        </div><!--END card info Row 1-->
+
+        <div class="form-row"><!--card info Row 2-->
+          <div class="col-md-4 mb-3">
+            <label>Credit Card Number</label>
+            <input type="text" class="form-control text-center" name="cardNum" id="" placeholder="XXXX-XXXX-XXXX-XXXX" required>
+          </div>
+          <div class="col-md-3 mb-3">
+            <label>Acceptable Cards</label>
             <img src="images/visa_logo.png"/>
             <img src="images/mastercard_logo.png"/>
           </div>
-
-          <div class="radio-input">
-            <input id="card" type="radio" name="payment">
-            Pay £340.00 with credit card
+        </div><!--END card info row 2-->
+        
+        <div class="form-row"><!--card info row 3-->
+          <div class="col-md-2 mb-3">
+            <label>CVV2</label>
+            <input type="text" class="form-control text-center" name="CVV2" id="" placeholder="XXX" required>
           </div>
-        </label>
-
-        <label for="paypal" class="method paypal">
-          <img src="images/paypal_logo.png"/>
-          <div class="radio-input">
-            <input id="paypal" type="radio" name="payment">
-            Pay £340.00 with PayPal
+          <div class="col-md-2 mb-3">
+            <label>Exp Date</label>
+            <input type="text" class="form-control text-center" name="expDate" id="" placeholder="XX/XX" required="">
+            
           </div>
-        </label>
-      </div>
+        </div><!--END card info row 3-->
+      </div><!--END payment-->
 
-      <div class="input-fields">
-        <div class="column-1">
-          <label for="cardholder">Cardholder's Name</label>
-          <input type="text" id="cardholder" />
+    </div><!--END Row1-->
 
-          <div class="small-inputs">
-            <div>
-              <label for="date">Valid thru</label>
-              <input type="text" id="date" placeholder="MM / YY" />
-            </div>
 
-            <div>
-              <label for="verification">CVV / CVC *</label>
-              <input type="password" id="verification"/>
-            </div>
-          </div>
 
-        </div>
-        <div class="column-2">
-          <label for="cardnumber">Card Number</label>
-          <input type="password" id="cardnumber"/>
-
-          <span class="info">* CVV or CVC is the card security code, unique three digits number on the back of your card separate from its number.</span>
-        </div>
-      </div>
-    </div>
-
-    <div class="panel-footer">
-      <button class="btn back-btn">Back</button>
-      <button class="btn next-btn">Next Step</button>
-    </div>
+  <div class="form-group text-center">
+    <button class="btn btn-secondary btn-rounded mt-3 buttonColor" type="submit" name="Checkout">Checkout</button>
   </div>
+ </form>
+</div> <!--CARD END-->
+<!--end of shipping page content-->
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="script.js"></script>
-</body>
-
-</div>
+<!-- validation script-->
+<script src="script/validation.js"></script>
 
 <!--Footer-->
 <?php
