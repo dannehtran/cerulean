6cbcf112c95d6436d17bd4a794a8d516c1e0171b
dannehtran <danny.tran.392@my.csun.edu>
2019-05-09 00:19:46 -0700

    Replaced old register.php file with AngularJS version

--- register.php
+++ register.php
@@ -31,27 +31,38 @@ if (@$_GET['error'] == 'UsernameOrEmailAlreadyTaken') {
 
  <!-- Sign Up Form -->
  <div class="card signUpForm shadow rounded border_color mx-5">
- <form method="POST" action="handlers/registerHandler.php" class="needs-validation py-4 px-4" novalidate>
+ <form method="POST" action="handlers/registerHandler.php" class="py-4 px-4" ng-submit="submitForm(regForm.$valid)" name="regForm" novalidate>
    <h1 class="text-center">Sign Up Form</h1>
 
    <!-- Row 1 -->
    <div class="form-row justify-content-center">
 
      <!-- Username Input Field -->
-     <div class="col-md-5 mb-3">
-       <label for="validationCustom01">Username</label>
-       <input type="text" class="form-control" name="username_reg" id="validationCustom01" placeholder="Username" required>
-       <div class="invalid-feedback">
-         Please enter a username.
+     <div class="col-md-5 mb-3" ng-class="{ 'help-block' : regForm.username_reg.$invalid && !regForm.username_reg.$pristine }">
+       <label>Username</label>
+       <input type="text" class="form-control" name="username_reg" ng-model="username_reg" id="validationCustom01" placeholder="Username" ng-minlength="4" ng-maxlength="10" required>
+       <div ng-show="regForm.username_reg.$invalid && !regForm.username_reg.$pristine" class="help-block">
+         <p ng-show="regForm.username_reg.$error.minlength" class="help-block">
+           Please enter at least 4 characters.
+         </p>
+         <p ng-show="regForm.username_reg.$error.maxlength" class="help-block">
+          Please enter less than 10 characters.
+         </p>
        </div>
      </div>
 
      <!-- Password Input Field -->
-     <div class="col-md-5 mb-3">
+
+     <div class="col-md-5 mb-3" ng-class="{ 'help-block' : regForm.password_reg.$invalid && !regForm.password_reg.$pristine }">
        <label for="validationCustom02">Password</label>
-       <input type="password" class="form-control" name="password_reg" id="validationCustom02" placeholder="Password" required>
-       <div class="invalid-feedback">
-         Please enter a password.
+       <input type="password" class="form-control" name="password_reg" id="validationCustom02" placeholder="Password" ng-model="password_reg" pw-check="password2_reg" ng-pattern="pattern" ng-minlength="9" required>
+       <div ng-show="regForm.password_reg.$invalid && !regForm.password_reg.$pristine" class="help-block">
+         <p ng-show="regForm.password_reg.$error.minlength" class="help-block">
+           Please enter at least 9 characters.
+         </p>
+         <p ng-show="regForm.password_reg.$error.pattern" class="help-block">
+           The password needs to have one lowercase, uppercase, number and special character.
+         </p>
        </div>
      </div>
    </div>
@@ -60,20 +71,24 @@ if (@$_GET['error'] == 'UsernameOrEmailAlreadyTaken') {
    <div class="form-row justify-content-center">
 
      <!-- Email Input Field -->
-     <div class="col-md-5 mb-3">
+     <div class="form-group col-md-5 mb-3" ng-class="{ 'help-block' : regForm.email_reg.$invalid && !regForm.email_reg.$pristine }">
        <label for="validationCustom03">Email</label>
-       <input type="text" class="form-control" name="email_reg" id="validationCustom03" placeholder="Email" required>
-       <div class="invalid-feedback">
-         Please enter your email address.
+       <input type="email" class="form-control" name="email_reg" id="validationCustom03" placeholder="Email" ng-model="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{3,}$" required>
+       <div>
+         <p ng-show="regForm.email_reg.$invalid && !regForm.email_reg.$pristine" class="help-block">
+           Please enter your email address.
+         </p>
        </div>
      </div>
 
      <!-- Confirm Password Input Field -->
-     <div class="col-md-5 mb-3">
-       <label for="validationCustom04">Re-type password</label>
-       <input type="password" class="form-control" name="password2_reg" id="validationCustom04" placeholder="Confirm Password" required>
-       <div class="invalid-feedback">
-         Please enter your password again.
+     <div class="col-md-5 mb-3" ng-class="{ 'help-block' : regForm.password2_reg.$invalid && !regForm.password2_reg.$pristine }">
+       <label>Confirm password</label>
+       <input type="password" class="form-control" ng-model="password2_reg" name="password2_reg" id="validationCustom04" placeholder="Confirm Password" required>
+       <div>
+         <p ng-show="regForm.password_reg.$error.pwmatch" class="help-block">
+         Passwords do not match!
+         </p>
        </div>
      </div>
    </div>
@@ -82,20 +97,24 @@ if (@$_GET['error'] == 'UsernameOrEmailAlreadyTaken') {
    <div class="form-row justify-content-center">
 
      <!-- First name Input Field -->
-     <div class="col-md-5 mb-3">
-       <label for="validationCustom05">First name</label>
-       <input type="text" class="form-control" name="first_name_reg" id="validationCustom05" placeholder="First name" required>
-       <div class="invalid-feedback">
-         Please enter your first name.
+     <div class="col-md-5 mb-3" ng-class="{ 'help-block' : regForm.first_name_reg.$invalid && !regForm.first_name_reg.$pristine }">
+       <label>First name</label>
+       <input type="text" class="form-control" name="first_name_reg" id="validationCustom05" placeholder="First name" ng-model="first_name_reg" pattern="[a-zA-Z]{2,25}" required>
+       <div>
+         <p ng-show="regForm.first_name_reg.$invalid && !regForm.first_name_reg.$pristine" class="help-block">
+           Please enter your first name.
+         </p>
        </div>
      </div>
 
      <!-- Lastname Input Field -->
-     <div class="col-md-5 mb-3">
-       <label for="validationCustom06">Last name</label>
-       <input type="text" class="form-control" name="last_name_reg" id="validationCustom06" placeholder="Last name" required>
-       <div class="invalid-feedback">
-         Please enter your last name.
+     <div class="col-md-5 mb-3" ng-class="{ 'help-block' : regForm.last_name_reg.$invalid && !regForm.last_name_reg.$pristine }">
+       <label>Last name</label>
+       <input type="text" class="form-control" name="last_name_reg" id="validationCustom06" placeholder="Last name" ng-model="last_name_reg" pattern="[a-zA-Z]{2,40}" required>
+       <div>
+         <p ng-show="regForm.last_name_reg.$invalid && !regForm.last_name_reg.$pristine" class="help-block">
+           Please enter your last name.
+         </p>
        </div>
      </div>
    </div>
@@ -104,39 +123,49 @@ if (@$_GET['error'] == 'UsernameOrEmailAlreadyTaken') {
    <div class="form-row justify-content-center">
 
      <!-- Address Input Field -->
-     <div class="col-md-6 mb-3">
-       <label for="validationCustom07">Address</label>
-       <input type="text" class="form-control" name="address_reg" id="validationCustom07" placeholder="Address" required>
-       <div class="invalid-feedback">
-         Please provide a valid address.
+     <div class="col-md-6 mb-3" ng-class="{ 'help-block' : regForm.address_reg.$invalid && !regForm.address_reg.$pristine }">
+       <label>Address</label>
+       <input type="text" class="form-control" name="address_reg" id="validationCustom07" placeholder="Address" ng-model="address_reg" pattern="[a-zA-Z0-9 ]{2,50}" required>
+       <div>
+         <p ng-show="regForm.address_reg.$invalid && !regForm.address_reg.$pristine" class="help-block">
+           Please provide an address.
+         </p>
        </div>
      </div>
 
      <!-- Address 2 Input Field -->
      <div class="col-md-4 mb-3">
        <label for="validationCustom08">Address 2 (Optional)</label>
-       <input type="text" class="form-control" name="address2_reg" id="validationCustom08" placeholder="Address2">
+       <input type="text" class="form-control" name="address2_reg" id="validationCustom08" placeholder="Address2" pattern="[a-zA-Z0-9 ]{2,50}">
      </div>
    </div>
 
    <!-- Row 5 -->
    <div class="form-row justify-content-center">
-     <div class="col-md-6 mb-3">
+     <div class="col-md-6 mb-3" ng-class="{ 'help-block' : regForm.city_reg.$invalid && !regForm.city_reg.$pristine }">
 
        <!-- City Input Field -->
-       <label for="validationCustom09">City</label>
-       <input type="text" class="form-control" name="city_reg" id="validationCustom09" placeholder="City" required>
-       <div class="invalid-feedback">
-         Please provide a valid city.
+       <label>City</label>
+       <input type="text" class="form-control" name="city_reg" id="validationCustom09" placeholder="City" ng-model="city_reg" pattern="[a-zA-Z ]{2,100}" required>
+       <div>
+         <p ng-show="regForm.city_reg.$invalid && !regForm.city_reg.$pristine" class="help-block">
+           Please enter a city.
+         </p>
        </div>
      </div>
 
      <!-- State Input Field -->
-     <div class="col-md-4 mb-3">
-       <label for="validationCustom10">State</label>
-       <input type="text" class="form-control" name="state_reg" id="validationCustom10" placeholder="State" required>
-       <div class="invalid-feedback">
-         Please provide a valid state.
+     <div class="col-md-4 mb-3" ng-class="{ 'help-block' : regForm.state_reg.$invalid && !regForm.state_reg.$pristine }">
+       <label>State</label>
+       <input type="state" class="form-control" name="state_reg" id="validationCustom10" placeholder="State" ng-model="state_reg" pattern="[A-Z]{2}" ng-minlength="2" ng-maxlength="2" required>
+       <div>
+         <p ng-show="regForm.state_reg.$invalid && !regForm.state_reg.$pristine" class="help-block">
+           Please enter a state (Example: CA).
+         </p>
+         <p ng-show="regForm.state_reg.$error.minlength" class="help-block">
+         </p>
+         <p ng-show="regForm.state_reg.$error.maxlength" class="help-block">
+         </p>
        </div>
      </div>
    </div>
@@ -145,41 +174,51 @@ if (@$_GET['error'] == 'UsernameOrEmailAlreadyTaken') {
    <div class="form-row justify-content-center">
 
      <!-- Phone number Input Field -->
-     <div class="col-md-5 mb-3">
-       <label for="validationCustom11">Phone Number</label>
-       <input type="text" class="form-control" name="phone_reg" id="validationCustom11" placeholder="Phone Number" required>
-       <div class="invalid-feedback">
-         Please provide a valid phone number.
+     <div class="col-md-5 mb-3" ng-class="{ 'help-block' : regForm.phone_reg.$invalid && !regForm.phone_reg.$pristine }">
+       <label>Phone Number</label>
+       <input type="number" class="form-control" name="phone_reg" id="validationCustom11" placeholder="Phone Number" ng-model="phone_reg" ng-minlength="10" ng-maxlength="11" required>
+       <div>
+         <p ng-show="regForm.phone_reg.$invalid && !regForm.phone_reg.$pristine" class="help-block">
+           Please enter a valid phone number.
+         </p>
+         <p ng-show="regForm.phone_reg.$error.minlength" class="help-block">
+         </p>
        </div>
      </div>
 
      <!-- Zipcode Input Field -->
-     <div class="col-md-5 mb-3">
-       <label for="validationCustom12">Zip</label>
-       <input type="text" class="form-control" name="zip_reg" id="validationCustom12" placeholder="Zip" required>
-       <div class="invalid-feedback">
-         Please provide a valid zip.
+     <div class="col-md-5 mb-3" ng-class="{ 'help-block' : regForm.zip_reg.$invalid && !regForm.zip_reg.$pristine }">
+       <label>Zip</label>
+       <input type="number" class="form-control" name="zip_reg" id="validationCustom12" placeholder="Zip" ng-model="zip_reg" ng-minlength="5" ng-maxlength="5" required>
+       <div>
+         <p ng-show="regForm.zip_reg.$invalid && !regForm.zip_reg.$pristine" class="help-block">
+           Please enter a valid zip code.
+         </p>
+         <p ng-show="regForm.zip_reg.$error.minlength && regForm.zip_reg.$error.maxlength " class="help-block">
+         </p>
        </div>
      </div>
    </div>
    <div class="form-group text-center">
      <div class="form-check">
-       <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-       <label class="form-check-label" for="invalidCheck">
+       <input class="form-check-input" type="checkbox" ng-model="checked" id="invalidCheck" required>
+       <label class="form-check-label">
          Agree to terms and conditions
        </label>
        <div class="invalid-feedback">
+         <p class="help-block" ng-show="validationmsg">
          You must agree before submitting.
+        </p>
        </div>
      </div>
-     <button class="btn btn-secondary btn-rounded mt-3 buttonColor" type="submit" action="handlers/registerHandler.php" name="register">Sign Up</button>
+     <button class="btn btn-secondary btn-rounded mt-3 buttonColor" type="submit" action="handlers/registerHandler.php" name="register" ng-disabled="regForm.$invalid" ng-click="checkvalidation();">Sign Up</button>
    </div>
  </form>
 </div>
 
 <!-- Validation Script -->
 
-<script src="script/validation.js"></script>
+
 
 <!-- Footer -->
 <?php
